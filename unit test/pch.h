7a62a4def75dd9a81017144ceb3c4ed7b25c734d
Thao Nguyen <nvthao147@gmail.com>
2026-03-17 11:18:24 +0100

    Remove merge conflict markers from unit tests

--- unit test/pch.h
+++ unit test/pch.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //
 // pch.h
 //
@@ -8,15 +7,4 @@
 #include "gtest/gtest.h"
 
 
-=======
-//
-// pch.h
-//
-
-#pragma once
-
-#include "gtest/gtest.h"
-
-
->>>>>>> 6799f4ae25297048bce00ca51bea490859e0e3b4
 //std::string MyFunction(int value);
